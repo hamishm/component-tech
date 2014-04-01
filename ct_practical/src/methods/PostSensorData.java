826cf9e9f2e94004b9e1e6bb49d7f37aac9a893d
marc <ms267@st-andrews.ac.uk>
2014-04-01 16:46:56 +0100

    one file

--- ct_practical/src/methods/PostSensorData.java
+++ ct_practical/src/methods/PostSensorData.java
@@ -1,23 +1,11 @@
 package methods;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.protocol.HTTP;
 
 import core.Network;
-import dataTypes.Location;
 import dataTypes.Response;
 import dataTypes.SensorData;
 
