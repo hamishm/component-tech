0ded417026ee77bb47a36c10de2403a173719b5d
Hamish Morrison <hm53@st-andrews.ac.uk>
2014-04-02 00:20:44 +0100

    broker: use strict_json for message decoding

--- broker.go
+++ broker.go
@@ -3,6 +3,7 @@ package main
 import (
     "component-tech/queue"
     "component-tech/rtree"
+    "component-tech/strict_json"
     "crypto/rand"
     "encoding/base64"
     "encoding/json"
@@ -14,7 +15,8 @@ import (
 
 
 const MaxRTreeNodes = 50
-
+const MinFillRatio = 0.35
+const DefaultMapSize = 500
 
 type Consumer struct {
     ID           string
@@ -29,8 +31,16 @@ func NewConsumer() *Consumer {
 }
 
 type Broker struct {
-    consumerTree *rtree.RTree
-    consumerMap  map[string]*Consumer
+    ConsumerTree *rtree.RTree
+    ConsumerMap  map[string]*Consumer
+}
+
+func newBroker() *Broker {
+    minFill := MaxRTreeNodes * MinFillRatio
+    return &Broker{
+        ConsumerTree: rtree.New(MaxRTreeNodes, int(minFill)),
+        ConsumerMap:  make(map[string]*Consumer, DefaultMapSize),
+    }
 }
 
 type RegisterConsumerMessage struct {
@@ -39,18 +49,27 @@ type RegisterConsumerMessage struct {
     Radius    float32 `json:"radius"`
 }
 
-func (b *Broker) handleRegConsumer(w http.ResponseWriter, r *http.Request, body []byte) {
-    var msg RegisterConsumerMessage
-    err := json.Unmarshal(body, &msg)
-    if err != nil {
-        http.Error(w, err.Error(), 400)
-        return
-    }
-
+func (msg *RegisterConsumerMessage) Bounds() rtree.Rect {
     left := msg.Longitude - msg.Radius
     right := msg.Longitude + msg.Radius
     top := msg.Latitude - msg.Radius
     bottom := msg.Latitude + msg.Radius
+
+    return rtree.Rect{
+        Left: left,
+        Top: top,
+        Bottom: bottom,
+        Right: right,
+    }
+}
+
+func (b *Broker) handleRegConsumer(w http.ResponseWriter, r *http.Request, body []byte) {
+    var msg RegisterConsumerMessage
+    err := strict_json.Unmarshal(body, &msg)
+    if err != nil {
+        http.Error(w, err.Error(), 400)
+        return
+    }
 
     var consID [16]byte
     _, err = rand.Read(consID[:])
@@ -60,16 +79,11 @@ func (b *Broker) handleRegConsumer(w http.ResponseWriter, r *http.Request, body 
     }
 
     consumer := NewConsumer()
-    consumer.Area = rtree.Rect{
-        Left: left,
-        Top: top,
-        Bottom: bottom,
-        Right: right,
-    }
+    consumer.Area = msg.Bounds()
     consumer.ID = base64.URLEncoding.EncodeToString(consID[:])
 
-    b.consumerMap[consumer.ID] = consumer
-    b.consumerTree.Insert(consumer, consumer.Area)
+    b.ConsumerMap[consumer.ID] = consumer
+    b.ConsumerTree.Insert(consumer, consumer.Area)
 
     w.Write([]byte(fmt.Sprintf("{\"consumer_id\": \"%s\"}\n", consumer.ID)))
 }
@@ -80,22 +94,21 @@ func (b *Broker) handleConsume(w http.ResponseWriter, r *http.Request, body []by
         http.Error(w, "Bad request for API", 400)
         return
     }
-    consumerID := pathParts[2]
 
-    consumer := b.consumerMap[consumerID]
-    msgs := consumer.MessageQueue.Poll()
-    w.Write([]byte("[\n  "))
-    for _, msg := range(msgs) {
-        sensorMsg := msg.(*QueuedMessage)
-        bytes, err := json.Marshal(sensorMsg)
-        if err != nil {
-            http.Error(w, "Error marshaling message", 500)
-            return
-        }
-        w.Write(bytes)
-        w.Write([]byte(",\n  "))
+    consumerID := pathParts[2]
+    consumer, ok := b.ConsumerMap[consumerID]
+    if !ok {
+        http.Error(w, "Consumer not registered", 400)
+        return
     }
-    w.Write([]byte("\n]\n"))
+
+    msgs := consumer.MessageQueue.Poll()
+    bytes, err := json.Marshal(msgs)
+    if err != nil {
+        http.Error(w, "Error marshaling messages", 500)
+    }
+
+    w.Write(bytes)
 }
 
 func (b *Broker) handleRegProducer(w http.ResponseWriter, r *http.Request, body []byte) {
@@ -108,13 +121,13 @@ type Location struct {
 }
 
 type ProducerMessage struct {
-    Location *Location      `json:"location"`
-    Data     []interface{} `json:"data"`
+    Location *Location                `json:"location"`
+    Data     []map[string]interface{} `json:"data"`
 }
 
 type QueuedMessage struct {
-    Location *Location
-    Data     interface{}
+    Location *Location              `json:"location"`
+    Data     map[string]interface{} `json:"data"`
 }
 
 func (b *Broker) handleProduce(w http.ResponseWriter, r *http.Request, body []byte) {
@@ -126,7 +139,7 @@ func (b *Broker) handleProduce(w http.ResponseWriter, r *http.Request, body []by
     //producerID := pathParts[1]
 
     var msg ProducerMessage
-    err := json.Unmarshal(body, &msg)
+    err := strict_json.Unmarshal(body, &msg)
     if err != nil {
         http.Error(w, err.Error(), 400)
         return
@@ -143,13 +156,14 @@ func (b *Broker) handleProduce(w http.ResponseWriter, r *http.Request, body []by
         }
     }
 
-    b.consumerTree.Visit(longitude, latitude, func(value interface{}, bounds rtree.Rect) {
+    b.ConsumerTree.Visit(longitude, latitude,func(value interface{}, bounds rtree.Rect) {
         consumer := value.(*Consumer)
         consumer.MessageQueue.PutMany(storedData)
     })
 
     w.Write([]byte("cheers"))
 }
+
 
 func (b *Broker) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     bodyBytes, err := ioutil.ReadAll(r.Body)
@@ -171,16 +185,6 @@ func (b *Broker) ServeHTTP(w http.ResponseWriter, r *http.Request) {
         http.Error(w, "Endpoint not found", 404)
     }
 }
-
-
-func newBroker() *Broker {
-    minFill := MaxRTreeNodes * 0.35
-    return &Broker{
-        consumerTree: rtree.New(MaxRTreeNodes, int(minFill)),
-        consumerMap:  make(map[string]*Consumer, 500),
-    }
-}
-
 
 func main() {
     broker := newBroker()
